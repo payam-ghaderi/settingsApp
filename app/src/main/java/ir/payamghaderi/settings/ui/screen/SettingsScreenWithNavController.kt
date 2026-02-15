9e7e27b8b6b5ff618be49e8bcd0ec0e80d621ba2
payam <52100017+atrocom@users.noreply.github.com>
2026-02-15 23:12:39 +0330

    second commit

---
+++ app/src/main/java/ir/payamghaderi/settings/ui/screen/SettingsScreenWithNavController.kt
@@ -0,0 +1,19 @@
+package ir.payamghaderi.settings.ui.screen
+
+import android.util.Log.i
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+ fun SettingsScreenWithNavController(
+    navController: NavHostController,
+    startDestination: String,
+    routes: List<String>,
+    innerPadding: PaddingValues
+) {
+
+}
